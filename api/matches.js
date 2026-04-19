694f134942e8e3b41fde61ceb5a008e84c171d73
jani681 <jani681@gmail.com>
2026-04-20 01:30:35 +0500

    Update handler to return static match data
    Replaced the fetch request with static response data for teams and score.

--- api/matches.js
+++ api/matches.js
@@ -1,23 +1,8 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch(
-      "https://cricbuzz-cricket2.p.rapidapi.com/mcenter/v1/100238/oversGraph",
-      {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key": "45de809bc2mshae44e2328038bd2p15397djsn434507fd5798",
-          "X-RapidAPI-Host": "cricbuzz-cricket2.p.rapidapi.com",
-        },
-      }
-    );
-
-    const data = await response.json();
-
-    res.status(200).json(data);
-  } catch (error) {
-    res.status(500).json({
-      error: "Something went wrong",
-      details: error.message,
-    });
-  }
+export default function handler(req, res) {
+  res.status(200).json({
+    team1: "Pakistan",
+    team2: "India",
+    score: "120/3",
+    overs: "15.2"
+  });
 }
