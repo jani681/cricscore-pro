74ff16784ea9f86059fc89e156f0bec2ad83abb5
jani681 <jani681@gmail.com>
2026-04-20 01:42:08 +0500

    Update and rename api/matches.js to matches.js

--- api/matches.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    team1: "Pakistan",
-    team2: "India",
-    score: "120/3",
-    overs: "15.2"
-  });
-}
---
+++ matches.js
@@ -0,0 +1,16 @@
+document.getElementById("loadBtn").addEventListener("click", () => {
+  const container = document.getElementById("matches");
+
+  const match = {
+    team1: "Pakistan",
+    team2: "India",
+    score: "120/3",
+    overs: "15.2"
+  };
+
+  container.innerHTML = `
+    <h3>${match.team1} vs ${match.team2}</h3>
+    <p><b>Score:</b> ${match.score}</p>
+    <p><b>Overs:</b> ${match.overs}</p>
+  `;
+});
